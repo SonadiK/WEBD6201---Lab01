26774b9794f5b07f3226f152eb4e448a438354cc
Sonadi Kannangara <ksonadi@gmail.com>
2022-02-11 12:56:18 -0500

    Added DisplayServicesPage() and DisplayAboutPage()

--- Scripts/app.js
+++ Scripts/app.js
@@ -136,9 +136,137 @@
         MainContent.appendChild(MainHeading4);
         MainContent.appendChild(MainParagraph3);
         MainContent.appendChild(MainImage3);
-
     }
 
+    /**
+     * The DisplayServicesPage() function loads the Services pages which consists of text and Images.
+     * It gives description about what different types of services we are providing.
+     */
+    function DisplayServicesPage()
+    {
+        // Step1: Get an entry point
+        console.log("Services Page");
+        let MainContent = document.getElementsByTagName("main")[0];
+        let DocumentBody = document.body;
+    
+        // Step2: Create elemnts for Heading, Paragraph and Image
+        let MainHeading1 = document.createElement("h1");
+        let MainHeading2 = document.createElement("h3");
+        let MainHeading3 = document.createElement("h3");
+        let MainHeading4 = document.createElement("h3");
+        let MainParagraph1 = document.createElement("p");
+        let MainParagraph2 = document.createElement("p");
+        let MainParagraph3 = document.createElement("p");
+        let MainImage1 = document.createElement("img");
+        let MainImage2 = document.createElement("img");
+        let MainImage3 = document.createElement("img");
+        
+        // Step3: Configure elements for heading
+        MainHeading1.setAttribute("id", "MainHeading1"); 
+        MainHeading1.setAttribute("class", "mt-3");
+        MainHeading1.textContent = "Our Services";
+        MainHeading2.setAttribute("id", "MainHeading2");
+        MainHeading2.setAttribute("class", "mt-4");
+        MainHeading2.textContent = "Web & Front-end Development";
+        MainHeading3.setAttribute("id", "MainHeading3");
+        MainHeading3.setAttribute("class", "mt-5");
+        MainHeading3.textContent = ".Net Development";
+        MainHeading4.setAttribute("id", "MainHeading4");
+        MainHeading4.setAttribute("class", "mt-5");
+        MainHeading4.textContent = "Database";
+
+        // Step4: Configure elements for Paragraph
+        MainParagraph1.setAttribute("id", "MainParagraph1");
+        MainParagraph1.textContent = ("To create a website which is creative as well as attractive or to upgrade already existing website, ");
+        MainParagraph1.textContent += ("we are providing the service which makes it possible by using different tools.");
+        MainParagraph2.setAttribute("id", "MainParagraph2");
+        MainParagraph2.textContent = (".Net Development is a open-source framework, used to develop form based as well as web-based applications.");
+        MainParagraph3.setAttribute("id", "MainParagraph3");
+        MainParagraph3.textContent = ("Database Development helps to assign data in a good way and organized.");
+
+        // Step5: Configure elements for Image
+        MainImage1.setAttribute("src", "./Images/webImg.jpg");
+        MainImage1.setAttribute("id", "MainImage1");
+        MainImage2.setAttribute("src", "./Images/net.jpg");
+        MainImage2.setAttribute("id", "MainImage2");
+        MainImage3.setAttribute("src", "./Images/database2.jpg");
+        MainImage3.setAttribute("id", "MainImage3"); 
+
+        // Step6: Insert all the elements to MainContent using appendChild
+        MainContent.appendChild(MainHeading1); 
+        MainContent.appendChild(MainHeading2);
+        MainContent.appendChild(MainParagraph1);
+        MainContent.appendChild(MainImage1);
+        MainContent.appendChild(MainHeading3);
+        MainContent.appendChild(MainParagraph2);
+        MainContent.appendChild(MainImage2);
+        MainContent.appendChild(MainHeading4);
+        MainContent.appendChild(MainParagraph3);
+        MainContent.appendChild(MainImage3); 
+    }
+
+    /**
+     * This DisplayAboutPage() function loads the About Us page.
+     * It gives information about the us, which includes our name, qualification, image and resume link.
+     */
+    function DisplayAboutPage()
+    {
+        // Step1: Get an entry point
+        console.log("About Page");
+        let MainContent = document.getElementsByTagName("main")[0];
+        let DocumentBody = document.body;
+
+        // Step2: Create elemnts for Heading, Paragraph and Image
+        let MainHeading5 = document.createElement("h1");
+        let MainHeading6 = document.createElement("h3");
+        let MainHeading7 = document.createElement("h3");
+        let MainParagraph4 = document.createElement("p");
+        let MainParagraph5 = document.createElement("p");
+        let MainImage4 = document.createElement("img");
+        let MainImage5 = document.createElement("img");
+        let MainLink = document.createElement("a");
+
+        // Step3: Configure elements for heading
+        MainHeading5.setAttribute("id", "MainHeading1"); 
+        MainHeading5.setAttribute("class", "mt-3");
+        MainHeading5.textContent = "About Us";
+        MainHeading6.setAttribute("id", "MainHeading2");
+        MainHeading6.setAttribute("class", "mt-4");
+        MainHeading6.textContent = "Sonadi Kannangara";
+        MainHeading7.setAttribute("id", "MainHeading3");
+        MainHeading7.setAttribute("class", "mt-5");
+        MainHeading7.textContent = "Devanshi Patel";
+
+        // Step4: Configure elements for Paragraph
+        MainParagraph4.setAttribute("id", "MainParagraph1");
+        MainParagraph4.textContent = ("2nd Year Student");
+        MainParagraph4.textContent += ("Computer Programming and Analysis");
+        MainParagraph5.setAttribute("id", "MainParagraph2");
+        MainParagraph5.textContent = ("2nd Year Student");
+        MainParagraph5.textContent += ("Computer Programming and Analysis");
+
+        // Step5: Configure elements for Image
+        MainImage4.setAttribute("src", "./Images/sonadi.jpeg");
+        MainImage4.setAttribute("id", "MainImage4");
+        MainImage5.setAttribute("src", "./Images/devanshi.jpeg");
+        MainImage5.setAttribute("id", "MainImage5");
+
+        // Step6: Configure elements for Link
+        MainLink.setAttribute("href", "./Resume/Resume.docx");
+        MainLink.setAttribute("id", "MainLink");
+        MainLink.textContent = ("Resume");
+
+        // Step6: Insert all the elements to MainContent using appendChild
+        MainContent.appendChild(MainHeading5); 
+        MainContent.appendChild(MainHeading7);
+        MainContent.appendChild(MainParagraph5);
+        MainContent.appendChild(MainImage5);
+        MainContent.appendChild(MainHeading6);
+        MainContent.appendChild(MainLink);
+        MainContent.appendChild(MainParagraph4);
+        MainContent.appendChild(MainImage4); 
+    }
+    
     // named function option
     function Start()
     {
